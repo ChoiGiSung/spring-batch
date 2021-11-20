13a9edf4fe086bbc5bb8944915c6abe871e3a39d
gisungchoi <chlrltjd159@naver.com>
2021-11-20 23:05:49 +0900

    feat : 배치 설정 추가

---
+++ src/main/java/com/spring/batch/BatchConfig.java
@@ -0,0 +1,10 @@
+package com.spring.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class BatchConfig {
+}
